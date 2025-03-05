fab5e41ced46f590693baf2ff7791fd0cb1b41da
bossr15 <fullstackwebsitedeveloper11@gmail.com>
2025-03-05 09:38:59 +0500

    Fixed user Stats card!

--- lib/presentation/view/user_management/components/user_stats.dart
+++ lib/presentation/view/user_management/components/user_stats.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grabby_babby_admin/core/utils/extension.dart';
 
 import '../../../../core/styles/app_color.dart';
 import '../../../../core/styles/app_images.dart';
@@ -12,50 +13,61 @@ class UserStats extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10),
       child: Container(
+        width: context.width * 0.9,
         padding: const EdgeInsets.symmetric(vertical: 20),
         decoration: BoxDecoration(
           color: AppColors.white,
           borderRadius: BorderRadius.circular(16),
         ),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              UserStatsCard(
-                image: AppImages.totalUsers,
-                title: 'Total Users',
-                value: '5,423',
-                showDivider: true,
-              ),
-              UserStatsCard(
-                image: AppImages.buyers,
-                title: 'Buyers',
-                value: '1,423',
-                showDivider: true,
-              ),
-              UserStatsCard(
-                image: AppImages.sellers,
-                title: 'Sellers',
-                value: '1,893',
-                showDivider: true,
-              ),
-              UserStatsCard(
-                image: AppImages.businessSellers,
-                title: 'Business Sellers',
-                value: '1,893',
-                showDivider: true,
-              ),
-              UserStatsCard(
-                image: AppImages.pendingApproval,
-                title: 'Pending Approval',
-                value: '189',
-                showDivider: false,
-              ),
-            ],
-          ),
+        child: Row(
+          children: [
+            UserStatsCard(
+              image: AppImages.totalUsers,
+              title: 'Total Users',
+              value: '5,423',
+            ),
+            _spacerDivider,
+            UserStatsCard(
+              image: AppImages.buyers,
+              title: 'Buyers',
+              value: '1,423',
+            ),
+            _spacerDivider,
+            UserStatsCard(
+              image: AppImages.sellers,
+              title: 'Sellers',
+              value: '1,893',
+            ),
+            _spacerDivider,
+            UserStatsCard(
+              image: AppImages.businessSellers,
+              title: 'Business Sellers',
+              value: '1,893',
+            ),
+            _spacerDivider,
+            UserStatsCard(
+              image: AppImages.pendingApproval,
+              title: 'Pending Approval',
+              value: '189',
+            ),
+            const SizedBox(width: 16),
+          ],
         ),
       ),
     );
   }
+
+  Widget get _spacerDivider => Expanded(
+        child: Row(
+          children: [
+            Spacer(),
+            Container(
+              height: 64,
+              width: 1,
+              color: AppColors.grey.withOpacity(0.5),
+            ),
+            Spacer(),
+          ],
+        ),
+      );
 }
